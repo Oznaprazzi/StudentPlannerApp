72940353c0a8930e77c9f422ec30f075c34f5296
Casey Huang <huangcase@ecs.vuw.ac.nz>
2018-07-24 23:51:49 +1200

    updated database relation schema

--- Documentation/Design/DatabaseDesign/DatabaseRelationalSchema.sql
+++ Documentation/Design/DatabaseDesign/DatabaseRelationalSchema.sql
@@ -1,20 +1,19 @@
 CREATE TYPE atype AS ENUM ('Assignment', 'Test', 'Exam', 'Other');
 
 CREATE TABLE Users(
-	UserId SERIAL PRIMARY KEY NOT NULL,
-	Username varchar(255) NOT NULL UNIQUE,
+	Username varchar(255) PRIMARY KEY NOT NULL,
 	Name varchar(255) NOT NULL,
 	Password varchar(255) NOT NULL
 );
 
 CREATE TABLE Lecturer(
 	LecturerId SERIAL PRIMARY KEY NOT NULL,
-	UserId int NOT NULL references Users(UserId) ON UPDATE CASCADE ON DELETE RESTRICT
+	Username varchar(255) NOT NULL references Users(Username) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Student(
-	StudentId SERIAL PRIMARY KEY NOT NULL,
-	UserId int NOT NULL references Users(UserId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	StudentId int PRIMARY KEY NOT NULL,
+	Username varchar(255) NOT NULL references Users(Username) ON UPDATE CASCADE ON DELETE CASCADE,
 	Points int NOT NULL DEFAULT 0
 );
 
@@ -25,8 +24,9 @@ CREATE TABLE Course(
 
 CREATE TABLE Assessment(
 	AssessmentId SERIAL PRIMARY KEY NOT NULL,
-	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE CASCADE,
 	AssessmentType atype NOT NULL,
+	StartDate date NOT NULL,
 	DueDate date NOT NULL CHECK(DueDate >= current_date),
 	Title varchar(255) NOT NULL,
 	Details text NOT NULL
@@ -34,34 +34,34 @@ CREATE TABLE Assessment(
 
 CREATE TABLE Task(
 	TaskId SERIAL PRIMARY KEY NOT NULL,
-	AssessmentId int NOT NULL references Assessment(AssessmentId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	AssessmentId int NOT NULL references Assessment(AssessmentId) ON UPDATE CASCADE ON DELETE CASCADE,
 	Name varchar(255) NOT NULL,
 	Points int NOT NULL DEFAULT 2,
 	Description text NOT NULL	
 );
 
 CREATE TABLE EnrolledIn (
-	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE CASCADE,
+	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY(StudentId, CourseId)
 );
 
 CREATE TABLE TaughtBy (
-	LecturerId int NOT NULL references Lecturer(LecturerId) ON UPDATE CASCADE ON DELETE RESTRICT,
-	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	LecturerId int NOT NULL references Lecturer(LecturerId) ON UPDATE CASCADE ON DELETE CASCADE,
+	CourseId int NOT NULL references Course(CourseId) ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY(LecturerId, CourseId)
 );
 
 CREATE TABLE CompletesAsessment (
-	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE RESTRICT,
-	AssessmentId int NOT NULL references Assessment(AssessmentId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE CASCADE,
+	AssessmentId int NOT NULL references Assessment(AssessmentId) ON UPDATE CASCADE ON DELETE CASCADE,
 	Completed boolean NOT NULL DEFAULT false,
 	PRIMARY KEY(StudentId, AssessmentId)
 );
 
 CREATE TABLE CompletesTask (
-	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE RESTRICT,
-	TaskId int NOT NULL references Task(TaskId) ON UPDATE CASCADE ON DELETE RESTRICT,
+	StudentId int NOT NULL references Student(StudentId) ON UPDATE CASCADE ON DELETE CASCADE,
+	TaskId int NOT NULL references Task(TaskId) ON UPDATE CASCADE ON DELETE CASCADE,
 	Completed boolean NOT NULL DEFAULT false,
 	PRIMARY KEY(StudentId, TaskId)
 );
